c9f55e959a53fff107b64e6c86ea19b1e6a5a468
ARBAAZ23 <69679866+ARBAAZ23@users.noreply.github.com>
2024-11-18 15:53:07 +0530

    Add files via upload

--- model.js
+++ model.js
@@ -11,3 +11,16 @@ cancelBtn.onclick = ()=>{
     menu.classList.remove("active");
     menuBtn.classList.remove("hide");
 }
+
+const profile = document.querySelector("#profile")
+const profileBtn = document.querySelector("#profile-icon")
+
+profileBtn.onclick = () => {
+    if (!profile.classList.contains("active")) {
+        profile.classList.add("active");
+        profileBtn.classList.add("profilehide");
+    } else {
+        profile.classList.remove("active");
+        profileBtn.classList.remove("profilehide");
+    }
+};
